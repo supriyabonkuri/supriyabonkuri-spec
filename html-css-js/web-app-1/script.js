2df99a0f04743c893b265088abc22164e3c7a906
supriyabonkuri-spec <supriyabonkuri@gmail.com>
2026-01-10 11:06:30 +0530

    first

---
+++ html-css-js/web-app-1/script.js
@@ -0,0 +1,8 @@
+function dispMessage(){
+   //a=10
+   //b=20
+   //alert(document.getElementById("txtEmail").value)
+    //alert(document.getElementById("pass").value)
+    //alert("Hello")
+    lbmsg.innerHTML = document.getElementById("txtEmail").value
+}
